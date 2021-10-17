b467b90ffc21dff58435407d2ff98dfe0fda4efa
Dion Tambunan <dion.20056@mhs.unesa.ac.id>
2021-10-18 00:58:34 +0700

    memperbaiki index

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title><?php echo $_SESSION['setting_hotel_name'] ?></title>
+        <title>Grand Amethyst</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
