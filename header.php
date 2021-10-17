f467be3fa8f6133a4ae5c013072ac4a5fc82f796
Dion Tambunan <dion.20056@mhs.unesa.ac.id>
2021-10-18 00:18:48 +0700

    file header.php untuk file index

---
+++ header.php
@@ -0,0 +1,21 @@
+ 		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php echo $_SESSION['setting_hotel_name'] ?></title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="asset/bootstrap-datepicker.css" rel="stylesheet" />
+        <link href="asset/styles.css" rel="stylesheet" />
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap-datepicker.js"></script>
+
