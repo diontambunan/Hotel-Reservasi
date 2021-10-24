35506b47ad11d9d08c4e5ad3ad8d1a749c733cbd
Dion Tambunan <dion.20056@mhs.unesa.ac.id>
2021-10-24 18:48:27 +0700

    pembaruan pada lama about.php

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
  <!-- Masthead-->
- <header class="masthead">
+        <header class="masthead">
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
@@ -13,7 +13,18 @@
 
     <section class="page-section">
         <div class="container">
-    <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>        
+      Grand Amethyst dengan bangga disandang oleh koleksi properti terkemuka yang terus bertambah di seluruh penjuru kota di Indonesia dan Asia. Sebagai grup hotel mewah baru di Indonesia, kami berkomitmen untuk menyediakan perjalanan tak terlupakan bagi para tamu kami yang terinspirasi oleh gaya Eropa yang indah. Kami percaya hidup harus dijalani dengan gaya.
+</br>
+</br>
+Setiap tahun, semakin banyak tamu yang datang untuk menghargai kualitas ini, karena Grand Amethyst menambahkan destinasi baru yang menarik di Indonesia dan Asia. Sementara pertumbuhan ini mencerminkan kekuatan dan kesuksesan merek Grand Amethyst, koleksinya akan tetap terbatas, di mana eksklusivitas dapat dipelihara dan individualitas dapat berkembang.
+</br>
+ Kami percaya bahwa warisan Eropa yang bergengsi menempatkan Grand Amethyst pada posisi yang unik untuk memenuhi harapan para pelancong yang penuh gaya dan cerdas. Bukan hanya sekadar menyediakan tempat tidur hotel dan makanan, di Grand Amethyst ini semua tentang menghidupkan cerita.
+</br>
+</br>
+ Sejak 2019, karyawan kami telah menjadi bagian dari penciptaan sejarah di seluruh dunia. Dari bangunan bersejarah hingga arsitektur modern paling avant-garde, properti kami adalah tempat untuk beberapa momen terbesar dalam hidup. Kami telah menyaksikan pertemuan bersejarah antara para pemimpin dunia, selebritas yang berlindung di dunia privasi yang kami ciptakan untuk mereka, dan menciptakan kenangan luar biasa bagi para tamu dalam perjalanan 'sekali seumur hidup'.
+</br>
+</br>
+Kami dengan sepenuh hati berkomitmen untuk memberikan kesempurnaan bagi para tamu kami di setiap saat dan dalam segala hal. Mungkin itu sesuatu yang mengubah hidup seperti perayaan pernikahan, di mana perhatian kami terhadap detail dan layanan sempurna berarti bahwa hari istimewa Anda berjalan tanpa hambatan. Atau mungkin sesederhana catatan yang tertinggal di bantal Anda di malam hari, yang dengan cermat memberi tahu Anda cuaca besok sehingga Anda tahu cara berpakaian di pagi hari. Di Grand Amethyst, kami masing-masing secara pribadi bertanggung jawab untuk menciptakan pengalaman yang kaya dan bermakna bagi para tamu kami.
             
         </div>
         </section>
